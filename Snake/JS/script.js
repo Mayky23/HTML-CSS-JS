e58c373f29a5f9adc3f48b3ee6a662262e5aedfc
Miguel Ángel Roldán de Haro <mayky23dh@gmail.com>
2023-05-31 11:12:35 +0200

    Ajustes de diseño
    Se modifica el apartado estético de CSS y se organiza el HTML en consecuencia. Se detecta un fallo persistente al centrar el tablero de juego.

--- Snake/JS/script.js
+++ Snake/JS/script.js
@@ -1,85 +1,90 @@
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
-const box = 20;
-const canvasSize = 20;
-let snake = [];
-snake[0] = { x: 10 * box, y: 10 * box };
-let food = {
-  x: Math.floor(Math.random() * canvasSize) * box,
-  y: Math.floor(Math.random() * canvasSize) * box
+const lienzo = document.getElementById("gameCanvas"); // Obtener el elemento del lienzo del juego
+const ctx = lienzo.getContext("2d"); // Obtener el contexto 2D del lienzo
+const caja = 20; // Tamaño de cada caja en el lienzo
+const tamanoLienzo = 20; // Tamaño del lienzo en número de cajas
+
+let serpiente = []; // Arreglo para almacenar los segmentos de la serpiente
+
+serpiente[0] = { x: 10 * caja, y: 10 * caja }; // Posición inicial de la cabeza de la serpiente
+
+let comida = { // Posición inicial de la comida
+  x: Math.floor(Math.random() * tamanoLienzo) * caja,
+  y: Math.floor(Math.random() * tamanoLienzo) * caja
 };
-let score = 0;
-let direction;
 
-document.addEventListener("keydown", changeDirection);
+let puntaje = 0; // Puntuación inicial
+let direccion;
 
-function changeDirection(event) {
-  const key = event.keyCode;
-  if (key === 37 && direction !== "RIGHT") direction = "LEFT";
-  else if (key === 38 && direction !== "DOWN") direction = "UP";
-  else if (key === 39 && direction !== "LEFT") direction = "RIGHT";
-  else if (key === 40 && direction !== "UP") direction = "DOWN";
+document.addEventListener("keydown", cambiarDireccion); // Agregar un evento para cambiar la dirección de la serpiente al presionar una tecla
+
+function cambiarDireccion(evento) {
+  const tecla = evento.keyCode; // Obtener el código de la tecla presionada
+  if (tecla === 37 && direccion !== "DERECHA") direccion = "IZQUIERDA"; // Si se presiona la flecha izquierda y la serpiente no se está moviendo hacia la derecha, cambiar la dirección a la izquierda
+  else if (tecla === 38 && direccion !== "ABAJO") direccion = "ARRIBA"; // Si se presiona la flecha arriba y la serpiente no se está moviendo hacia abajo, cambiar la dirección hacia arriba
+  else if (tecla === 39 && direccion !== "IZQUIERDA") direccion = "DERECHA"; // Si se presiona la flecha derecha y la serpiente no se está moviendo hacia la izquierda, cambiar la dirección hacia la derecha
+  else if (tecla === 40 && direccion !== "ARRIBA") direccion = "ABAJO"; // Si se presiona la flecha abajo y la serpiente no se está moviendo hacia arriba, cambiar la dirección hacia abajo
 }
 
-function collision(newHead, snake) {
-  for (let i = 0; i < snake.length; i++) {
-    if (newHead.x === snake[i].x && newHead.y === snake[i].y) {
-      return true;
+function colision(nuevaCabeza, serpiente) {
+  for (let i = 0; i < serpiente.length; i++) { // Verificar si la nueva cabeza de la serpiente colisiona con cualquiera de sus segmentos
+    if (nuevaCabeza.x === serpiente[i].x && nuevaCabeza.y === serpiente[i].y) {
+      return true; // Si hay colisión, devolver true
     }
   }
-  return false;
+  return false; // Si no hay colisión, devolver false
 }
 
-function draw() {
-  ctx.fillStyle = "#000";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+function dibujar() {
+  ctx.fillStyle = "#000"; // Establecer el color de fondo del lienzo
+  ctx.fillRect(0, 0, lienzo.width, lienzo.height); // Dibujar un rectángulo para llenar todo el lienzo
 
-  for (let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = "#00ff00";
-    ctx.fillRect(snake[i].x, snake[i].y, box, box);
+  for (let i = 0; i < serpiente.length; i++) { // Dibujar cada segmento de la serpiente
+    ctx.fillStyle = "#00ff"; // Establecer el color de la serpiente
+    ctx.fillRect(serpiente[i].x, serpiente[i].y, caja, caja); // Dibujar un rectángulo para representar cada segmento
   }
 
-  ctx.fillStyle = "#ff0000";
-  ctx.fillRect(food.x, food.y, box, box);
+  ctx.fillStyle = "#ff0000"; // Establecer el color de la comida
+  ctx.fillRect(comida.x, comida.y, caja, caja); // Dibujar un rectángulo para representar la comida
 
-  ctx.fillStyle = "#fff";
-  ctx.font = "20px Arial";
-  ctx.fillText("Score: " + score, box, box);
+  ctx.fillStyle = "#fff"; // Establecer el color del texto
+  ctx.font = "20px Arial"; // Establecer el tamaño y la fuente del texto
+  ctx.fillText("Puntaje: " + puntaje, caja, caja); // Dibujar el puntaje en el lienzo
 
-  let snakeX = snake[0].x;
-  let snakeY = snake[0].y;
+  let cabezaX = serpiente[0].x; // Obtener la coordenada X de la cabeza de la serpiente
+  let cabezaY = serpiente[0].y; // Obtener la coordenada Y de la cabeza de la serpiente
 
-  if (direction === "LEFT") snakeX -= box;
-  if (direction === "UP") snakeY -= box;
-  if (direction === "RIGHT") snakeX += box;
-  if (direction === "DOWN") snakeY += box;
+  if (direccion === "IZQUIERDA") cabezaX -= caja; // Actualizar la coordenada X de la cabeza de la serpiente según la dirección
+  if (direccion === "ARRIBA") cabezaY -= caja; // Actualizar la coordenada Y de la cabeza de la serpiente según la dirección
+  if (direccion === "DERECHA") cabezaX += caja; // Actualizar la coordenada X de la cabeza de la serpiente según la dirección
+  if (direccion === "ABAJO") cabezaY += caja; // Actualizar la coordenada Y de la cabeza de la serpiente según la dirección
 
-  if (snakeX === food.x && snakeY === food.y) {
-    score++;
-    food = {
-      x: Math.floor(Math.random() * canvasSize) * box,
-      y: Math.floor(Math.random() * canvasSize) * box
+  if (cabezaX === comida.x && cabezaY === comida.y) { // Verificar si la serpiente ha alcanzado la comida
+    puntaje++; // Incrementar el puntaje
+    comida = { // Generar una nueva posición para la comida de forma aleatoria
+      x: Math.floor(Math.random() * tamanoLienzo) * caja,
+      y: Math.floor(Math.random() * tamanoLienzo) * caja
     };
   } else {
-    snake.pop();
+    serpiente.pop(); // Si no se alcanzó la comida, eliminar el último segmento de la serpiente para simular el movimiento
   }
 
-  const newHead = {
-    x: snakeX,
-    y: snakeY
+  const nuevaCabeza = { // Crear una nueva cabeza para la serpiente en la nueva posición
+    x: cabezaX,
+    y: cabezaY
   };
 
   if (
-    snakeX < 0 ||
-    snakeY < 0 ||
-    snakeX >= canvasSize * box ||
-    snakeY >= canvasSize * box ||
-    collision(newHead, snake)
+    cabezaX < 0 || // Verificar si la serpiente ha colisionado con los bordes del lienzo
+    cabezaY < 0 ||
+    cabezaX >= tamanoLienzo * caja ||
+    cabezaY >= tamanoLienzo * caja ||
+    colision(nuevaCabeza, serpiente) // Verificar si la serpiente ha colisionado consigo misma
   ) {
-    clearInterval(game);
+    clearInterval(juego); // Detener el juego
   }
 
-  snake.unshift(newHead);
+  serpiente.unshift(nuevaCabeza); // Agregar la nueva cabeza a la serpiente
+
 }
 
-let game = setInterval(draw, 150);
+let juego = setInterval(dibujar, 150); // Ejecutar la función dibujar cada 150 milisegundos para actualizar el juego
