d45c4883e2876be94ccc28e91b5a4b1a21c5aacb
Miguel Ángel Roldán de Haro <mayky23dh@gmail.com>
2023-06-05 11:01:20 +0200

    Juego base finalizado
    Se consigue implementar todas las mecánicas y que todos los elementos funcionen correctamente, aunque el proyecto queda abierto a más actualizaciones.

--- Snake/JS/script.js
+++ Snake/JS/script.js
@@ -1,4 +1,6 @@
 const contenedorJuego = document.getElementById("container_game");
+const botonJugar = document.getElementById("botonJugar");
+const botonReiniciar = document.getElementById("botonReiniciar");
 const caja = 20; // Tamaño de cada caja en el juego
 const tamanoLienzo = 20; // Tamaño del juego en número de cajas
 
@@ -9,10 +11,12 @@ let comida = { // Posición inicial de la comida
 };
 
 let puntaje = 0; // Puntuación inicial
-let direccion = "DERECHA"; // Dirección inicial de la serpiente
+let direccion = "ARRIBA"; // Dirección inicial de la serpiente
 let juego; // Variable para almacenar el intervalo del juego
 
-document.addEventListener("keydown", cambiarDireccion); // Agregar un evento para cambiar la dirección de la serpiente al presionar una tecla
+botonJugar.addEventListener("click", iniciarJuego);
+botonReiniciar.addEventListener("click", reiniciarJuego);
+document.addEventListener("keydown", cambiarDireccion);
 
 function cambiarDireccion(evento) {
   const tecla = evento.keyCode; // Obtener el código de la tecla presionada
@@ -39,6 +43,7 @@ function dibujar() {
     segmento.style.width = caja + "px";
     segmento.style.height = caja + "px";
     segmento.style.backgroundColor = "#00ff";
+    segmento.style.borderRadius = "5px";
     segmento.style.position = "absolute";
     segmento.style.left = serpiente[i].x + "px";
     segmento.style.top = serpiente[i].y + "px";
@@ -49,13 +54,14 @@ function dibujar() {
   comidaElemento.style.width = caja + "px";
   comidaElemento.style.height = caja + "px";
   comidaElemento.style.backgroundColor = "#ff0000";
+  comidaElemento.style.borderRadius = "20px";
   comidaElemento.style.position = "absolute";
   comidaElemento.style.left = comida.x + "px";
   comidaElemento.style.top = comida.y + "px";
   contenedorJuego.appendChild(comidaElemento);
 
   const puntajeElemento = document.getElementById("puntaje"); // Obtener el elemento del puntaje
-  puntajeElemento.textContent = "Puntos: " + puntaje; // Actualizar el puntaje en el elemento
+  puntajeElemento.textContent = "POINTS: " + puntaje; // Actualizar el puntaje en el elemento
 
   let cabezaX = serpiente[0].x; // Obtener la coordenada X de la cabeza de la serpiente
   let cabezaY = serpiente[0].y; // Obtener la coordenada Y de la cabeza de la serpiente
@@ -65,20 +71,29 @@ function dibujar() {
   if (direccion === "DERECHA") cabezaX += caja; // Actualizar la coordenada X de la cabeza de la serpiente según la dirección
   if (direccion === "ABAJO") cabezaY += caja; // Actualizar la coordenada Y de la cabeza de la serpiente según la dirección
 
+  // Verificar si la serpiente ha colisionado con los bordes del juego
+  if (
+    cabezaX < 0 ||
+    cabezaY < 0 ||
+    cabezaX >= tamanoLienzo * caja ||
+    cabezaY >= tamanoLienzo * caja ||
+    colision({ x: cabezaX, y: cabezaY }, serpiente)
+  ) {
+    clearInterval(juego); // Detener el juego
+    const recordGuardado = localStorage.getItem("record");
+    const recordActual = parseInt(recordGuardado) || 0;
+    if (puntaje > recordActual) {
+      localStorage.setItem("record", puntaje.toString()); // Guardar el nuevo récord en el localStorage
+      const recordElemento = document.getElementById("record");
+      recordElemento.textContent = puntaje.toString(); // Actualizar el récord en la página
+    }
+    return;
+  }
+
   const nuevaCabeza = { // Crear una nueva cabeza para la serpiente en la nueva posición
     x: cabezaX,
     y: cabezaY
   };
-
-  if (
-    cabezaX < 0 || // Verificar si la serpiente ha colisionado con los bordes del juego
-    cabezaY < 0 ||
-    cabezaX >= tamanoLienzo * caja ||
-    cabezaY >= tamanoLienzo * caja ||
-    colision(nuevaCabeza, serpiente) // Verificar si la serpiente ha colisionado consigo misma
-  ) {
-    clearInterval(juego); // Detener el juego
-  }
 
   serpiente.unshift(nuevaCabeza); // Agregar la nueva cabeza a la serpiente
 
@@ -94,27 +109,17 @@ function dibujar() {
 }
 
 function iniciarJuego() {
-  serpiente = []; // Reiniciar la serpiente
-  serpiente[0] = { x: 10 * caja, y: 10 * caja }; // Posición inicial de la cabeza de la serpiente
+  serpiente = [{ x: 200, y: 200 }]; // Posición inicial de la serpiente
   puntaje = 0; // Reiniciar el puntaje
-  direccion = "DERECHA"; // Dirección inicial de la serpiente
-  juego = setInterval(dibujar, 300); // Iniciar el juego con un intervalo de 150 milisegundos
+  direccion = "ARRIBA"; // Reiniciar la dirección
+  juego = setInterval(dibujar, 220); // Llamar a la función dibujar cada 200 milisegundos
 }
 
 function reiniciarJuego() {
-  clearInterval(juego); // Detener el juego actual
-  iniciarJuego(); // Iniciar un nuevo juego
+  clearInterval(juego); // Detener el juego
+  const recordElemento = document.getElementById("record");
+  recordElemento.textContent = localStorage.getItem("record") || "0"; // Mostrar el récord actual almacenado en el localStorage
+  iniciarJuego(); // Iniciar el juego nuevamente
 }
 
-// Obtener los elementos de los botones
-const botonJugar = document.getElementById("botonJugar");
-const botonReiniciar = document.getElementById("botonReiniciar");
-
-// Agregar eventos a los botones
-botonJugar.addEventListener("click", iniciarJuego);
-botonReiniciar.addEventListener("click", reiniciarJuego);
-
-// Iniciar el juego al cargar la página
-window.onload = function () {
-  iniciarJuego();
-};
+iniciarJuego(); // Iniciar el juego
